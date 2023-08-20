be2a0e68bfc3c2ad26d511d914c815c43af9ee6f
ahmed <memosaid091@gmail.com>
2023-08-20 17:34:26 +0300

    update

--- warehouse system.py
+++ warehouse system.py
@@ -1,5 +1,5 @@
 class product_informations:
-    def __inti__(self):
+    def __init__(self):
         self.product_name = input("enter the product name : ")
         self.product_class = input("enter the product class : ")
         self.product_amount_by_dozen = int(input("enter how many dozan you want add : "))
@@ -10,26 +10,31 @@ class product_informations:
         self.date = "-".join(self._date_lst)
 
     product_list = []
-    def warehouse_add(self,product):
-        self.product_list.append(product)
+    def warehouse_add(cls,product):
+        cls.product_list.append(product)
 
-    def authentication(self,name):
-        for i in range(len(self.product_list)):
-            if name in self.product_list[i].product.values():
-                print()
-                print("Authentication successful!")
-                return self.product_list[i]
+    def authentication(cls,name):
+        for product in cls.product_list:
 
+            if name == product['name']:
+                print("\nAuthentication successful!")
+
+                return product
+
+        return None
 
 class warehouse (product_informations):
-    product = {}
+    product_list = []
     print("you are now in the warehouse ")
     def __init__(self):
 
-        super().__inti__()
+        super().__init__()
+
+        self.product = {}
         self.product['name'] = self.product_name
         self.product['info'] = (self.product_class,self.product_amount_by_dozen
                                     ,self.product_dozan_price,self.date,self.product_dozan_price_sale)
+        self.warehouse_add(self.product)
     def check(self):
         print(f"""information of {self.product['name']} product class ,product amount by dozen,
 product dozan price,product dozan price sale,Expiry date on the following order :\n {self.product['info']}""")
@@ -55,7 +60,7 @@ while running:
     4. Exit
     """)
 
-    choice = int(input("1, 2 or 3: "))
+    choice = int(input("1, 2, 3 or 4: "))
     if choice == 1:
         print()
         c1 = warehouse()
@@ -63,26 +68,31 @@ while running:
         print("done")
     elif choice == 2:
         name = input("enter your product name : ")
-        current_product = c1.authentication(name)
-        if current_product:
-            com_in = True
-            while com_in:
-                c1.export()
-        else:
+        try:
+            current_product = c1.authentication(name)
+            if current_product:
+                com_in = True
+                while com_in:
+                    c1.export()
+                    break
+        except:
             print("the product not found")
     elif choice == 3:
         name = input("enter your product name : ")
-        current_product = c1.authentication(name)
-        if current_product:
-            com_in = True
-            while com_in:
-                print()
-                c1.check()
-        else:
+        try:
+            current_product = c1.authentication(name)
+            if current_product:
+                com_in = True
+                while com_in:
+                    print()
+                    c1.check()
+                    break
+        except:
             print("the product not found")
 
     elif choice == 4:
         print()
         print("Goodbye!")
         running = False
+    continue
 
